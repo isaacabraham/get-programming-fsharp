0510eebcc320f9cd72f173607b3760750a65e631
Isaac Abraham <isaac.abraham@gmail.com>
2016-09-20 20:12:26 +0200

    Lesson 23.

---
+++ lesson-23/CSharpApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.FSharp.Core;
+using Model;
+using System;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(4, "Supacars", Tuple.Create(1.5, 3.5));
+            var bike = Vehicle.NewMotorbike("MyBike", 1.5);
+            var motorcar = Vehicle.NewMotorcar(car);
+
+            Functions.Describe(bike);
+            Functions.Describe(motorcar);
+
+            var fourWheeledNormal = Functions.CreateCar(4, "Supacars", 1.5, 3.5);
+            var fourWheeledFSharpFunc =
+                Functions.CreateFourWheeledCar
+                         .Invoke("Supacars")
+                         .Invoke(1.5)
+                         .Invoke(3.5);
+
+            // Working with Options
+            var optionalCar = FSharpOption<Car>.Some(car);
+            var isNone = FSharpOption<Car>.get_IsNone(optionalCar);
+            var isNotSome = FSharpOption<Car>.get_IsSome(optionalCar);
+
+            // With extension methods
+            var optionalCarExt = car.AsOption();
+            var isNoneExt = optionalCarExt.IsNone();
+            var isSomeExt = optionalCarExt.IsSome();
+        }
+    }
+
+    public static class OptionExtensions
+    {
+        public static bool IsSome<T>(this FSharpOption<T> option) => FSharpOption<T>.get_IsSome(option);
+        public static bool IsNone<T>(this FSharpOption<T> option) => FSharpOption<T>.get_IsNone(option);
+        public static FSharpOption<T> AsOption<T>(this T item)
+        {
+            if (item == null)
+                return FSharpOption<T>.None;
+            return FSharpOption<T>.Some(item);
+        }
+    }
+}
---
+++ lesson-23/FSharpCode/Library1.fs
@@ -0,0 +1,29 @@
+﻿namespace Model
+
+//[<CLIMutable>] // experiment with this
+/// A standard F# record of a Car.
+type Car =
+    { /// The number of wheels on the car.
+      Wheels : int
+      /// The brand of the car.
+      Brand : string
+      /// The x/y of the car in meters
+      Dimensions : float * float }
+
+/// A vehicle of some sort.
+type Vehicle =
+  /// A car is a type of vehicle.
+| Motorcar of Car
+  /// A bike is also a type of vehicle.
+| Motorbike of Name:string * EngineSize:float
+
+module Functions =
+    /// Describes a vehicle.
+    let Describe vehicle =
+        match vehicle with
+        | Motorcar c -> printfn "This is a car with %d wheels!" c.Wheels
+        | Motorbike(_, size) -> printfn "This is a bike with engine %f" size
+
+    let CreateCar wheels brand x y = { Wheels = wheels; Brand = brand; Dimensions = x, y }
+    /// Creates a car with four wheels.
+    let CreateFourWheeledCar = CreateCar 4
