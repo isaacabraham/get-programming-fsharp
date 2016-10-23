f02f42537a250dbd6045e1ee204bb92f29a3b0f4
Isaac Abraham <isaac.abraham@gmail.com>
2016-10-23 18:11:10 +0200

    Lesson 27

---
+++ src/code-listings/lesson-27-1.fsx
@@ -0,0 +1,50 @@
+#r @"..\..\packages\FSharp.Data.SqlClient\lib\net40\FSharp.Data.SqlClient.dll"
+
+open FSharp.Data
+
+// Listing 27.1
+let [<Literal>] Conn = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
+type GetCustomers = SqlCommandProvider<"SELECT * FROM SalesLT.Customer WHERE CompanyName = @CompanyName", Conn>
+let customers = GetCustomers.Create(Conn).Execute("A Bike Store") |> Seq.toArray
+let customer = customers.[0]
+
+// Now you try #1
+printfn "%s %s works for %s" customer.FirstName customer.LastName (customer.CompanyName |> defaultArg <| "no-one")
+
+// Now you try #2
+type AdventureWorks = SqlProgrammabilityProvider<Conn>
+let productCategory = new AdventureWorks.SalesLT.Tables.ProductCategory()
+productCategory.AddRow("Mittens", Some 3)
+productCategory.AddRow("Long Shorts", Some 3)
+productCategory.AddRow("Wooly Hats", Some 4)
+productCategory.Update()
+
+// Listing 27.2
+type Categories = SqlEnumProvider<"SELECT Name, ProductCategoryId FROM SalesLT.ProductCategory", Conn>
+let woolyHats = Categories.``Wooly Hats``
+printfn "Wooly Hats has ID %d" woolyHats
+
+
+
+
+
+
+
+
+
+
+
+
+let processData (conn:string) =
+    let getCustomers = new SqlCommandProvider<"SELECT TOP 10 * FROM SalesLT.Customer", Conn>(conn)
+    let customers = getCustomers.Execute() |> Seq.toArray
+    customers.[0]
+
+processData Conn
+
+type AdventureWorks = SqlProgrammabilityProvider<Conn>
+let conn = new System.Data.SqlClient.SqlConnection(Conn)
+conn.Open()
+let Q = AdventureWorks.CreateCommand<"SELECT * FROM SalesLT.Customer">(conn).Execute() |> Seq.toArray
+
+let z = AdventureWorks.CreateCommand<"SELECT * FROM SalesLT.Customer">
---
+++ src/code-listings/lesson-27-2.fsx
@@ -0,0 +1,31 @@
+// Listing 27.3
+#r @"..\..\packages\SQLProvider\lib\FSharp.Data.SqlProvider.dll"
+open FSharp.Data.Sql
+
+type AdventureWorks = SqlDataProvider<ConnectionString = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI", UseOptionTypes = true>
+
+let context = AdventureWorks.GetDataContext()
+
+type Customer = { Name : string }
+
+let customers = 
+    query {
+        for customer in context.SalesLt.Customer do
+        take 10
+    } |> Seq.toArray
+let customer = customers.[0]
+
+// Listing 27.4
+let names = 
+    query {
+        for customer in context.SalesLt.Customer do
+        where (customer.CompanyName = Some "Sharp Bikes")
+        select { Name = (customer.FirstName + " " + customer.LastName) }
+        distinct
+    } |> Seq.toArray
+
+// Listing 27.5
+let category = context.SalesLt.ProductCategory.Create()
+category.ParentProductCategoryId <- Some 3
+category.Name <- "Scarf"
+context.SubmitUpdates()
