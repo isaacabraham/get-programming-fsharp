b8add29328a6120ca39d32fb552ad526f8fbc2e9
Isaac Abraham <isaac.abraham@gmail.com>
2016-11-26 14:14:55 +0100

    Lesson 29.

---
+++ src/code-listings/lesson-29/SqlDemo/Program.fs
@@ -0,0 +1,15 @@
+﻿open FSharp.Data
+
+let [<Literal>] Conn = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
+type GetCustomers = SqlCommandProvider<"SELECT TOP 50 * FROM SalesLT.Customer", Conn>
+
+[<EntryPoint>]
+let main _ = 
+    let customers = GetCustomers.Create(Conn)
+    
+    customers.Execute()
+    |> Seq.iter (fun c -> printfn "%A: %s %s" c.CompanyName c.FirstName c.LastName)
+    
+    0
+
+//    let liveConnection = System.Configuration.ConfigurationManager.ConnectionStrings.["AdventureWorks"].ConnectionString
