0e6c4f1046912df3b802488d6ed602b802747c9b
Isaac Abraham <isaac.abraham@gmail.com>
2016-12-29 17:47:07 +0100

    Update API.

--- src/code-listings/lesson-29/Core/Api.fs
+++ src/code-listings/lesson-29/Core/Api.fs
@@ -5,20 +5,24 @@ open Capstone5.Domain
 open Capstone5.Operations
 open System
 
-/// Deposits funds into an account.
-let Deposit (amount:decimal) (ratedAccount:RatedAccount) : RatedAccount =
-    ratedAccount
-
-/// Withdraws funds from an account that is in credit.
-let Withdraw (amount:decimal) (creditAccount:CreditAccount) : RatedAccount =
-    InCredit creditAccount
-    
 /// Loads the transaction history for an owner. If no transactions exist, returns an empty sequence.
-let LoadTransactionHistory(owner:string) : Transaction seq =
+let LoadTransactionHistory(customer:Customer) : Transaction seq =
     Seq.empty
 
-/// Loads an account from disk. If no account exists, an empty one is automatically created.
-let LoadAccount(owner:string) : RatedAccount =
+/// Deposits funds into an account.
+let Deposit (amount:decimal) (customer:Customer) : RatedAccount =
     InCredit(CreditAccount { AccountId = Guid.NewGuid()
                              Balance = 0M
-                             Owner = { Name = owner } })
+                             Owner = customer })
+
+/// Withdraws funds from an account that is in credit.
+let Withdraw (amount:decimal) (customer:Customer) : RatedAccount =
+    InCredit(CreditAccount { AccountId = Guid.NewGuid()
+                             Balance = 0M
+                             Owner = customer })
+                                 
+/// Loads an account from disk. If no account exists, an empty one is automatically created.
+let LoadAccount (customer:Customer) : RatedAccount =
+    InCredit(CreditAccount { AccountId = Guid.NewGuid()
+                             Balance = 0M
+                             Owner = customer })
