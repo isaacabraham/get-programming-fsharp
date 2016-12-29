93837146a08b3b01336bdda9024f27ba48ac8914
Isaac Abraham <isaac.abraham@gmail.com>
2016-12-29 15:47:45 +0100

    Basic solution structure.

---
+++ src/code-listings/lesson-29/Core/Api.fs
@@ -0,0 +1,24 @@
+﻿/// Provides access to the banking API.
+module Capstone5.Api
+
+open Capstone5.Domain
+open Capstone5.Operations
+open System
+
+/// Deposits funds into an account.
+let Deposit (amount:decimal) (ratedAccount:RatedAccount) : RatedAccount =
+    ratedAccount
+
+/// Withdraws funds from an account that is in credit.
+let Withdraw (amount:decimal) (creditAccount:CreditAccount) : RatedAccount =
+    InCredit creditAccount
+    
+/// Loads the transaction history for an owner.
+let LoadTransactionHistory(owner:string) : Transaction array =
+    Array.empty
+
+/// Loads an account from disk. If no account exists, an empty one is automatically created.
+let LoadAccount(owner:string) : RatedAccount =
+    InCredit(CreditAccount { AccountId = Guid.NewGuid()
+                             Balance = 0M
+                             Owner = { Name = owner } })
