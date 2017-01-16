a349146e79ae20791142443c78179ea720266460
Isaac Abraham <isaac.abraham@gmail.com>
2017-01-16 10:23:40 +0100

    Remove 404 messages.

--- src/code-listings/lesson-37/SuaveApp/Program.fs
+++ src/code-listings/lesson-37/SuaveApp/Program.fs
@@ -13,16 +13,16 @@ let asJson mapper =
         mapper
 
 /// Converts a result to a WebPart
-let asResponse res notFoundMessage =
+let asResponse res =
     match res with
     | Some (Controllers.Success result) -> OK (result |> JsonConvert.SerializeObject)
     | Some (Controllers.Failure errorMessage) -> Suave.RequestErrors.BAD_REQUEST errorMessage
-    | None -> Suave.RequestErrors.NOT_FOUND notFoundMessage
+    | None -> Suave.RequestErrors.NOT_FOUND ""
 
 /// Wraps the getAnimal function 
 let getAnimal name ctx = async {
     let! animal = Controllers.AnimalsRepository.getAnimal name
-    return! asResponse animal "No such animal" ctx }
+    return! asResponse animal ctx }
 
 let app logger =
     choose [
@@ -30,7 +30,7 @@ let app logger =
             choose [
                 path "/api/animals" >=> (Controllers.AnimalsRepository.getAll |> asJson)
                 pathScan "/api/animals/%s" getAnimal ]
-        Suave.RequestErrors.NOT_FOUND "This is the default HTTP 404 page." ]
+        Suave.RequestErrors.NOT_FOUND "" ]
         >=> log logger logFormat
 
 [<EntryPoint>]
