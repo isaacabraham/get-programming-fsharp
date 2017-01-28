1f405fe4ea61415a7d4ea22fa43e5e11c601bfa8
Isaac Abraham <isaac.abraham@gmail.com>
2017-01-28 14:36:59 +0000

    Lesson 40.

---
+++ src/code-listings/lesson-40/UnitTesting/UnitTesting/BusinessLogic.fs
@@ -0,0 +1,11 @@
+﻿// Listing 40.1
+module BusinessLogic
+
+type Employee = { Name : string; Age : int }
+type Department = { Name : string; Team : Employee list } 
+
+let isLargeDepartment department = department.Team.Length > 10
+let isLessThanTwenty person = person.Age < 20
+let isLargeAndYoungTeam department =
+    department |> isLargeDepartment
+    && department.Team |> List.forall isLessThanTwenty
---
+++ src/code-listings/lesson-40/UnitTesting/UnitTesting/BusinessLogicTests.fs
@@ -0,0 +1,46 @@
+﻿module ``Business Logic Tests``
+
+open BusinessLogic
+
+open Xunit
+
+let department =
+    { Name = "Super Team"
+      Team = [ for i in 1 .. 15 -> { Name = sprintf "Person %d" i; Age = 19 } ] }
+
+// Listing 40.2
+[<Fact>]
+let ``Large, young teams are correctly identified``() =
+    Assert.True(department |> isLargeAndYoungTeam)
+
+// Listing 40.3
+let isTrue (b:bool) = Assert.True b
+
+[<Fact>]
+let ``Custom DSLs are easy to write``() =
+    department
+    |> isLargeAndYoungTeam
+    |> isTrue
+
+// Listing 40.4
+open FsUnit.Xunit
+[<Fact>]
+let ``FSUnit makes nice DSLs!``() =
+    department
+    |> isLargeAndYoungTeam
+    |> should equal true
+
+    department.Team.Length
+    |> should be (greaterThan 10)
+
+// Listing 40.5
+open Swensen.Unquote
+[<Fact>]
+let ``Unquote has a simple custom operator for equality``() =
+    department |> isLargeAndYoungTeam =! true
+
+// Listing 40.6
+[<Fact>]
+let ``Unquote can parse quotations for excellent diagnostics``() =
+    let emptyTeam = { Name = "Super Team"; Team = [] }
+    test <@ emptyTeam.Name.StartsWith "D" @>
