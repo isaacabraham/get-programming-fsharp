b7e5f5426b196e5c2d17c686b88c817db0f404c9
Isaac Abraham <isaac.abraham@gmail.com>
2017-03-15 09:48:43 +0100

    Correct Lesson 12 sample.

--- src/code-listings/lesson-12/Operations.fs
+++ src/code-listings/lesson-12/Operations.fs
@@ -2,5 +2,5 @@
 
 open Domain
 
-let getInitials customer = customer.FirstName.[0] + customer.LastName.[0]
+let getInitials customer = customer.FirstName.[0], customer.LastName.[0]
 let isOlderThan age customer = customer.Age > age
