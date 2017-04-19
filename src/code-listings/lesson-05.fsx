c4d4d9ca05ed81c4b06497661ced48ea01cd5d0a
Isaac Abraham <isaac.abraham@gmail.com>
2017-04-19 12:38:15 +0200

    Fix Lesson 5 listings.

--- src/code-listings/lesson-05.fsx
+++ src/code-listings/lesson-05.fsx
@@ -1,14 +1,14 @@
-// Listing 5.7
+// Listing 5.6
 let add (a:int, b:int) : int =
     let answer:int = a + b
     answer
 
-// Listing 5.11
+// Listing 5.7
 let getLength name = sprintf "Name is %d letters." name.Length
 let getLength (name:string) = sprintf "Name is %d letters." name.Length
 let foo(name) = "Hello! " + getLength(name)
 
-// Listing 5.13
+// Listing 5.8
 open System.Collections.Generic
 let numbers = List<_>()
 numbers.Add(10)
@@ -18,14 +18,14 @@ let otherNumbers = List()
 otherNumbers.Add(10)
 otherNumbers.Add(20)
 
-// Listing 5.14
+// Listing 5.9
 let createList(first, second) =
     let output = List()
     output.Add(first)
     output.Add(second)
     output
 
-// Listing 5.15
+// Listing 5.10
 let sayHello(someValue) =
     let innerFunction(number) =
         if number > 10 then "Isaac"
