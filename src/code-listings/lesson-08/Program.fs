3c72c0320c4c49a1e15cd45f4e62fc32c7b999df
Isaac Abraham <isaac.abraham@gmail.com>
2017-04-19 13:13:32 +0200

    Update Lesson 8 (consistency).

--- src/code-listings/lesson-08/Program.fs
+++ src/code-listings/lesson-08/Program.fs
@@ -11,10 +11,9 @@ let mutable petrol = 100
 let main argv =
     while true do
         try
-        let destination = getDestination()
-        printfn "Trying to drive to %s" destination
-        petrol <- driveTo(petrol, destination)
-        printfn "Made it to %s! You have %d petrol left" destination petrol
-        with
-        ex -> printfn "ERROR: %s" ex.Message
+            let destination = getDestination()
+            printfn "Trying to drive to %s" destination
+            petrol <- driveTo(petrol, destination)
+            printfn "Made it to %s! You have %d petrol left" destination petrol
+        with ex -> printfn "ERROR: %s" ex.Message
     0
