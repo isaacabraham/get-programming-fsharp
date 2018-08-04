979e4a91ae37ff7015571c9645fd854bb6342ec0
Isaac Abraham <isaac.abraham@gmail.com>
2018-08-04 12:41:10 +0200

    Lesson 26

--- src/code-listings/lesson-26/NugetFSharp/Script1.fsx
+++ src/code-listings/lesson-26/NugetFSharp/Script1.fsx
@@ -1,9 +1,9 @@
-﻿#I @"..\packages\"
-#r @"Humanizer.Core.2.1.0\lib\netstandard1.0\Humanizer.dll"
-#r @"Newtonsoft.Json.9.0.1\lib\net45\Newtonsoft.Json.dll"
-#load "Library1.fs"
+﻿// Change this to where your packages folder is located!
+#I @"C:\users\isaac\.nuget\packages\"
 
-//#load @"Scripts\load-project-debug.fsx"
+#r @"Humanizer.Core\2.1.0\lib\netstandard1.0\Humanizer.dll"
+#r @"Newtonsoft.Json\9.0.1\lib\netstandard1.0\Newtonsoft.Json.dll"
+#load "Library1.fs"
 
 open Humanizer
 "ScriptsAreAGreatWayToExplorePackages".Humanize(LetterCasing.AllCaps)
