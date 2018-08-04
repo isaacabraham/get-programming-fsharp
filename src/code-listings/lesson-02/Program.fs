5f481bb907f8e11221ed5c30e4b6e9241c780478
Isaac Abraham <isaac.abraham@gmail.com>
2018-08-04 11:44:46 +0200

    Initial work to allow working on netcore

---
+++ src/code-listings/lesson-02/Program.fs
@@ -0,0 +1,9 @@
+﻿// You can run this application using e.g.
+// dotnet run -- banana apple orange
+
+[<EntryPoint>]
+let enhancedMain argv =     
+    let items = argv.Length     
+    printfn "Passed in %d items: %A" items argv     
+    0 // return an integer exit code 
+
